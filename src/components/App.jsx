d4959556085f03ba6d2d82fa235a9b9c3027bed3
irynatrysh <98460886+irynatrysh@users.noreply.github.com>
2023-12-05 12:37:23 -0500

    final code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
 export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  const handleSubmit = ({ name, number }) => {
+    if (contacts.some((contact) => contact.name === name)) {
+      alert(`${name} is already in contacts!`);
+      return;
+    }
+
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    setContacts((prevContacts) => [...prevContacts, newContact]);
+  };
+
+  const handleDelete = (id) => {
+    setContacts((prevContacts) => prevContacts.filter((contact) => contact.id !== id));
+  };
+
+  const handleChangeFilter = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="phonebook-frame">
+      <h1>Phonebook</h1>
+
+      <ContactForm onSubmit={handleSubmit} />
+
+      <h2>Contacts</h2>
+
+      <Filter value={filter} onChange={handleChangeFilter} />
+
+      <ContactList contacts={filteredContacts} onDelete={handleDelete} />
     </div>
   );
 };
